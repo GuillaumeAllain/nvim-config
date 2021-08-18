1c79692b47107042cfdd61e5f255262ad0d001f1
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-08-18 13:05:18 -0400

    added correct color for folded

--- lua/plugin_config/theme.lua
+++ lua/plugin_config/theme.lua
@@ -3,19 +3,14 @@ vim.cmd[[let &fcs='eob: ']]
 vim.cmd[[set termguicolors]]
 vim.cmd[[set noshowmode]]
 
--- vim.cmd[[hi Normal guibg=none ctermbg=none]]
---
-vim.cmd[[hi! LineNr guibg=none ctermbg=none]]
-vim.cmd[[hi! Folded guibg=none ctermbg=none]]
-vim.cmd[[hi! NonText guibg=none ctermbg=none]]
-vim.cmd[[hi! SpecialKey guibg=none ctermbg=none]]
-vim.cmd[[hi! VertSplit guibg=none ctermbg=none]]
-vim.cmd[[hi! SignColumn guibg=none ctermbg=none]]
-vim.cmd[[hi! EndOfBuffer guibg=none ctermbg=none]]
-
-vim.cmd[[
+vim.cmd[[hi! LineNr guibg=none ctermbg=none
+hi! Folded guibg=none ctermbg=none
+hi! NonText guibg=none ctermbg=none
+hi! SpecialKey guibg=none ctermbg=none
+hi! VertSplit guibg=none ctermbg=none
+hi! SignColumn guibg=none ctermbg=none
+hi! EndOfBuffer guibg=none ctermbg=none
 hi! link Conceal Comment
 hi! link FoldColumn Comment
+hi! link Folded Comment
 ]]
-
--- hi! link LineNr Comment

61afdfff23c322fe57565cbdf7be137dd2775b85
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-10-18 22:42:04 -0400

    added pandoc

--- lua/plugin_config/sandwich.lua
+++ lua/plugin_config/sandwich.lua
@@ -2,6 +2,17 @@ vim.cmd([[
 let g:sandwich#recipes = deepcopy(g:sandwich#default_recipes)
 let g:sandwich#recipes += [
   \   {
+  \ '__filetype__': 'pandoc',
+  \     'buns': ['{', '}'],
+  \     'cursor': 'head',
+  \     'command': ['startinsert'],
+  \     'kind': ['add', 'replace'],
+  \     'action': ['add'],
+  \     'input': ['f']
+  \   },
+  \ ]
+let g:sandwich#recipes += [
+  \   {
   \ '__filetype__': 'tex',
   \     'buns': ['{', '}'],
   \     'cursor': 'head',

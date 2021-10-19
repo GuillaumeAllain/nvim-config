5fbc1b74181510ca36aeed10464ff386cbb5146c
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-10-18 22:41:24 -0400

    added function for pandoc and latex;;

---
+++ lua/plugin_config/sandwich.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+let g:sandwich#recipes = deepcopy(g:sandwich#default_recipes)
+let g:sandwich#recipes += [
+  \   {
+  \ '__filetype__': 'tex',
+  \     'buns': ['{', '}'],
+  \     'cursor': 'head',
+  \     'command': ['startinsert'],
+  \     'kind': ['add', 'replace'],
+  \     'action': ['add'],
+  \     'input': ['f']
+  \   },
+  \ ]
+]])

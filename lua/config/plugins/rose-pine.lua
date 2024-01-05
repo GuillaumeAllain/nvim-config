5352c627dd83e119e48409f022abaafd3a5fee26
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2024-01-05 13:09:36 -0500

    change rose-pine

--- lua/config/plugins/rose-pine.lua
+++ lua/config/plugins/rose-pine.lua
@@ -2,6 +2,7 @@ return {
     "rose-pine/neovim",
     lazy = false,
     priority = 1000,
+    name = "rose-pine",
     config = function()
         require("rose-pine").setup({
             dark_variant = "moon",

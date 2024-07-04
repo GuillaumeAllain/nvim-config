7ec650ff9b1295f6845a6f78e124bb39cb5c7e68
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2024-07-03 22:32:41 -0400

    cleanup

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,9 +1,26 @@
 return {
-    "echasnovski/mini.nvim",
-    lazy = false,
-    config = function()
-        require("mini.indentscope").setup()
-        vim.cmd([[autocmd TermOpen * lua vim.b.miniindentscope_disable = true]])
-        require("mini.comment").setup({options={ignore_blank_lines=true}})
-    end,
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            require("mini.indentscope").setup()
+            vim.cmd([[autocmd TermOpen * lua vim.b.miniindentscope_disable = true]])
+            require("mini.comment").setup({ options = { ignore_blank_lines = true } })
+        end,
+    },
+    {
+        "echasnovski/mini.icons",
+        opts = {},
+        lazy = true,
+        specs = {
+            { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+        },
+        init = function()
+            package.preload["nvim-web-devicons"] = function()
+                -- needed since it will be false when loading and mini will fail
+                package.loaded["nvim-web-devicons"] = {}
+                require("mini.icons").mock_nvim_web_devicons()
+                return package.loaded["nvim-web-devicons"]
+            end
+        end,
+    },
 }

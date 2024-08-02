4506a52deb91d44a211eaf8ab66e823ff9c46056
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2024-08-02 16:47:51 -0400

    update to fzf

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -5,6 +5,10 @@ return {
             "nvim-lua/popup.nvim",
         },
         {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        },
+        {
             "folke/todo-comments.nvim",
         },
         {
@@ -83,13 +87,13 @@ return {
                 },
             },
             extensions = {
-                -- fzf = {
-                --     fuzzy = true, -- false will only do exact matching
-                --     override_generic_sorter = false, -- override the generic sorter
-                --     override_file_sorter = true, -- override the file sorter
-                --     case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                --     -- the default case_mode is "smart_case"
-                -- },
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
                 file_browser = {
                     mappings = {
                         i = {

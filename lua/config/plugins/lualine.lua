02f792f1fc5b3ab089fa5971b850b131e4ad8d84
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2025-05-19 10:47:06 -0400

    cleaning lualien

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -29,46 +29,6 @@ return {
             end,
         }
 
-        -- custom_term.normal.a.bg = vim.g["terminal_color_6"]
-        -- custom_term.normal.a.fg = vim.g["terminal_color_0"]
-        -- custom_term.inactive.a.bg = vim.g["terminal_color_6"]
-        -- custom_term.inactive.a.fg = vim.g["terminal_color_0"]
-        -- custom_term.insert.a.bg = vim.g["terminal_color_15"]
-        -- custom_term.insert.a.fg = vim.g["terminal_color_0"]
-        -- custom_term.visual.a.bg = vim.g["terminal_color_14"]
-        -- custom_term.visual.a.fg = vim.g["terminal_color_0"]
-        -- custom_term.replace.a.bg = vim.g["terminal_color_3"]
-        -- custom_term.replace.a.fg = vim.g["terminal_color_0"]
-        -- custom_term.command.a.bg = vim.g["terminal_color_15"]
-        -- custom_term.command.a.fg = vim.g["terminal_color_0"]
-
-        -- custom_term.normal.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.normal.b.fg = vim.g["terminal_color_15"]
-        -- custom_term.inactive.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.inactive.b.fg = vim.g["terminal_color_15"]
-        -- custom_term.insert.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.insert.b.fg = vim.g["terminal_color_15"]
-        -- custom_term.visual.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.visual.b.fg = vim.g["terminal_color_15"]
-        -- custom_term.replace.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.replace.b.fg = vim.g["terminal_color_15"]
-        -- custom_term.command.b.bg = vim.g["terminal_color_0"]
-        -- custom_term.command.b.fg = vim.g["terminal_color_15"]
-
-        -- custom_term.normal.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.normal.c.fg = vim.g["terminal_color_7"]
-        -- custom_term.inactive.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.inactive.c.fg = vim.g["terminal_color_7"]
-        -- custom_term.insert.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.insert.c.fg = vim.g["terminal_color_7"]
-        -- custom_term.visual.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.visual.c.fg = vim.g["terminal_color_7"]
-        -- custom_term.replace.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.replace.c.fg = vim.g["terminal_color_7"]
-        -- custom_term.command.c.bg = vim.g["terminal_color_0"]
-        -- custom_term.command.c.fg = vim.g["terminal_color_7"]
-        -- vim.cmd([[hi! MsgArea ctermbg=0 guibg=]] .. vim.g["terminal_color_0"])
-
         require("lualine").setup({
             options = {
                 -- theme = custom_term,
@@ -98,7 +58,7 @@ return {
                     {
                         function()
                             local msg = "No Active Lsp"
-                            local buf_ft = vim.api.nvim_get_option_value(0, "filetype")
+                            local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
                             local clients = vim.lsp.get_clients()
                             if next(clients) == nil then
                                 return msg

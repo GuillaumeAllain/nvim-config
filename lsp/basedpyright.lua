5aab595e2e6c5a5466589e932ce1d67424cd09d9
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2025-06-16 21:40:53 -0400

    update

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,8 +1,5 @@
 return {
     "basedpyright",
-    -- handlers = {
-    --     ["textDocument/publishDiagnostics"] = function() end,
-    -- },
     on_init = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentFormattingRangeProvider = false

2332ea8b397895b680166e61e1954a5ee4e128a0
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2025-06-17 22:27:11 -0400

    delete basedpyright in lsp

--- lsp/basedpyright.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "basedpyright",
-    on_init = function(client)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentFormattingRangeProvider = false
-    end,
-}

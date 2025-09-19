492957f849d66f4e2e408530746b82d8aae47e18
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2025-09-19 18:10:57 -0400

    remove unused

--- after/lsp/basedpyright.lua
+++ after/lsp/basedpyright.lua
@@ -1,7 +1,4 @@
 return {
-    -- handlers = {
-    --     ["textDocument/publishDiagnostics"] = function() end,
-    -- },
     on_init = function(client)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentFormattingRangeProvider = false

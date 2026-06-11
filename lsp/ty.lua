c2350b2027b0606af7198f4d23e08d34b373a899
Guillaume Allain <13963457+GuillaumeAllain@users.noreply.github.com>
2026-06-11 09:20:47 -0400

    update default ti

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,4 +1,9 @@
 return {
     cmd = { "ty", "server" },
     filetypes = { "python" },
+    init_options = {
+        environment = {
+            python = ".pixi/envs/default/bin/python",
+        },
+    },
 }
